3f8a23352a4ccad9a744170042dbe99caf1c5b9c
pabloem <polecito.em@gmail.com>
2016-05-27 11:42:59 +0900

    First release candidate of UI

--- resources/ui/js/checkStatus.js
+++ resources/ui/js/checkStatus.js
@@ -1,14 +1,20 @@
 (function() {
+  var displayStatus = function(jwDict) {
+    var node = new PrettyJSON.view.Node({ 
+      el:$("#statusResultSpan"), 
+      data:jwDict
+            });
+  };
   var getStatus = function(callback) {
     var res = {},
         done = 0;
     $.get("/jobs","",function(data){ res['jobs'] = JSON.parse(data);
                                      done += 1;
-                                     if(done == 2 && callback) callback();
+                                     if(done == 2 && callback) callback(res);
                                    });
     $.get("/workers","",function(workers){ res['workers'] = JSON.parse(workers);
                                            done += 1;
-                                           if(done == 2 && callback) callback(); });
+                                           if(done == 2 && callback) callback(res); });
   };
-  $("#statusLink").click(getStatus);
+  $("#statusLink").click(function(){getStatus(displayStatus);});
 }());
--- resources/ui/js/createJob.js
+++ resources/ui/js/createJob.js
@@ -21,10 +21,16 @@
           // If the field is required, but absent: problem!
           if (requiredFields[e.id] == true &&
               (e.value === undefined || e.value == "")) {
-            console.log(e.id + " is a required field.");
             missingFields += 1;
           }
           dict[e.id] = e.value;
+        });
+        var keys = $("#jobform .extrajobattr"),
+            vals = $("#jobform .extrajobval");
+        vals.each(function(i,v) {
+          var k = keys[i];
+          if(!k.value || k.value == "") return;
+          dict[k.value] = v.value;
         });
         readyJob = true;
         if(missingFields > 0) readyJob = false;
@@ -32,21 +38,36 @@
       },
       refreshJobDef = function() {
         var dict = getJobDef();
-        console.log(dict);
+        var node = new PrettyJSON.view.Node({
+          data:dict,
+          el:$("#createJobDefSpan")
+          });
       },
       submitJobDef = function() {
         if(!readyJob) {
           console.log("Job can't be submitted. The definition is not ready.");
           return;
         }
-        console.log("Posting job creation...");
         $.post("/jobs", JSON.stringify(getJobDef()),
                function(){alert("success");});
       }
   $("#jobform .jobdata").change(refreshJobDef);
   $("#createButton").click(submitJobDef);
+  $("#resetButton").click(function(){$("#jobform .jobdata").val(""); 
+                                     $("#jobform .extrajobdata").remove();
+                                     refreshJobDef();});
 
   /* Code for extra job attributes (key:value pairs to add at job creation)
    */
-  $("#addKeyValButton").click(function(){console.log("IMPLEMENT ADD K/V PAIR");});
+  var extraAppendString = 
+'<div class="pure-control-group">\
+    <input id="" class="extrajobdata extrajobattr" placeholder="Attribute"></textarea>\
+    <input id="" class="extrajobdata extrajobval" placeholder="Value"></textarea>\
+</div>';
+  var addKeyValue = function() {
+    $("#newKVPairs")
+      .append(extraAppendString)
+      .change(refreshJobDef);
+  };
+  $("#addKeyValButton").click(addKeyValue);
 }());
